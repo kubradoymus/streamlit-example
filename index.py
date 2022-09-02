3efa1ff2e65e9e92cf8837e30b63227bca172d70
kubraprojeler <112731826+kubraprojeler@users.noreply.github.com>
2022-09-03 01:30:16 +0300

    Update index.py

--- index.py
+++ index.py
@@ -5,9 +5,3 @@ from functions import *
 st.subheader("Günün Randevuları")   #altbaslık
 gununrandevu()
 
-
-#direkt hesap yapıyor.
-#tarih=st.date_input("tarih gir")
-#kubra=datetime.date(2022,8,28)
-#st.write(kubra-tarih)
-#sırada randevu tablosu yap.
